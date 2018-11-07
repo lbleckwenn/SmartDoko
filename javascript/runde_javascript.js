2ae696cc6741cea80c8395bcd45907a1abe5fb70
Lars Bleckwenn <lars.bleckwenn@web.de>
2018-11-07 15:24:58 +0100

    Entwicklungsupdate

--- javascript/runde_javascript.js
+++ javascript/runde_javascript.js
@@ -1,5 +1,193 @@
+// Initialisierung
+
+var spielTyp;
+var anzeigeAnsagen = true;
+var anzeigeSonderpunkt = true;
+var anzeigeStilleHochzeit = true;
+var auswahl = 'spieler';
+var spieler;
+var partner;
+var gegner;
+var sonderpunkt;
+var players = alleSpielerIds;
+$('#menu').html(createMenu('Auswahl:', players, [ 'abrechnung', 'beenden' ]));
+
+$(document).on("click", '#buttons > button.btn', function(event) {
+	var buttonId = this.id;
+	var solos = {
+		kreuzsolo : 5,
+		piksolo : 6,
+		herzsolo : 7,
+		karosolo : 8,
+		damensolo : 9,
+		bubensolo : 10,
+		fleischlos : 11
+	};
+	if (buttonId in solos) {
+		spielTyp = solos[buttonId];
+		$('#menu').html(createMenu('', [], getPossibleButtons()));
+		return;
+	}
+	sonderpunkte = {
+		doppelkopf : 0,
+		fuchsgefangen : 1,
+		karlchen : 0,
+		karlchengefangen : 1
+	};
+	if (buttonId in sonderpunkte) {
+		sonderpunkt = buttonId;
+		
+		$('#menu').html(createMenu('', [], getPossibleButtons()));
+		return;
+	}
+	if ($.isNumeric(buttonId)) {
+		switch (auswahl) {
+		case 'spieler':
+			spieler = buttonId;
+			break;
+		case 'partner':
+			partner = buttonId;
+			break;
+		case 'gegner':
+			gegner = buttonId;
+			break;
+		}
+		if (spielTyp) {
+			$('#menu').html(createMenu('', [], getPossibleButtons()));
+		} else {
+			$('#menu').html(createMenu('Bitte auswählen:', [], [ 're', 'kontra', 'sonderpunkt', 'hochzeit', 'armut', 'solo', 'abbrechen' ]));
+		}
+	}
+	switch (buttonId) {
+	case 're':
+	case 'kontra':
+		// Spieler hat Re oder Kontra angesagt
+		spielTyp = 1;
+		anzeigeAnsagen = false;
+		anzeigeSonderpunkt = false;
+		anzeigeStilleHochzeit = false;
+		// Anzeige Absageauswahl
+		break;
+	case 'hochzeit':
+		spielTyp = 2;
+		auswahl = 'partner';
+		anzeigeSonderpunkt = false;
+		anzeigeStilleHochzeit = false;
+		players = getPlayersWithout(spieler);
+		$('#menu').html(createMenu('Mitspieler auswählen', players, [ 'abbrechen' ]));
+		break;
+	case 'armut':
+		spielTyp = 4;
+		auswahl = 'partner';
+		anzeigeSonderpunkt = false;
+		anzeigeStilleHochzeit = false;
+		players = getPlayersWithout(spieler);
+		$('#menu').html(createMenu('Mitspieler auswählen', players, [ 'abbrechen' ]));
+		break;
+	case 'solo':
+		anzeigeSonderpunkt = false;
+		anzeigeStilleHochzeit = false;
+		$('#menu').html(createMenu('Solo auswählen', [], [ 'kreuzsolo', 'piksolo', 'herzsolo', 'karosolo', 'damensolo', 'bubensolo', 'fleischlos', 'abbrechen' ]));
+		break;
+	case 'sonderpunkt':
+		$('#menu').html(createMenu('Sonderpunkt auswählen', [], [ 'doppelkopf', 'fuchsgefangen', 'karlchen', 'karlchengefangen', 'abbrechen' ]));
+	}
+});
+$(document).on("click", '#buttons > button.btn', function(event) {
+	var buttonId = this.id;
+});
+function createMenu(text, players, buttons) {
+	var retvar = '<span id="buttons">';
+	if (text) {
+		retvar += '<span class="font-weight-bold">' + text + '</span>';
+	}
+	for (i = 0; i < players.length; i++) {
+		retvar += createButton(players[i]);
+	}
+	for (i = 0; i < buttons.length; i++) {
+		retvar += createButton(buttons[i]);
+	}
+	retvar += debug();
+	retvar += '</span>';
+	return retvar;
+}
+function debug() {
+	var retvar = '<dl class="row mt-3">';
+	retvar += '<dt class="col-7">spielTyp</dt><dd class="col-5">' + spielTyp + '</dd>';
+	retvar += '<dt class="col-7">spieler</dt><dd class="col-5">' + spieler + '</dd>';
+	retvar += '<dt class="col-7">partner</dt><dd class="col-5">' + partner + '</dd>';
+	retvar += '<dt class="col-7">gegner</dt><dd class="col-5">' + gegner + '</dd>';
+	retvar += '<dt class="col-7">sonderpunkt</dt><dd class="col-5">' + sonderpunkt + '</dd>';
+	retvar += '<dt class="col-7">anzeigeAnsagen</dt><dd class="col-5">' + anzeigeAnsagen + '</dd>';
+	retvar += '<dt class="col-7">anzeigeSonderpunkt</dt><dd class="col-5">' + anzeigeSonderpunkt + '</dd>';
+	retvar += '<dt class="col-7">anzeigeStilleHochzeit</dt><dd class="col-5">' + anzeigeStilleHochzeit + '</dd>';
+	retvar += '</dl>';
+	return retvar;
+}
 function createButton(button) {
-	var buttons = {
+	var buttons = allButtons();
+	if ($.isNumeric(button)) {
+		buttons[button] = {
+			farbe : 'btn-primary',
+			text : alleSpieler[button]
+		};
+	}
+	return '<button type="button" class="btn ' + buttons[button].farbe + ' btn-block" id="' + button + '">' + buttons[button].text + '</button>';
+}
+function getPlayersWithout(player) {
+	var players = [];
+	for (i = 0; i < alleSpielerIds.length; i++) {
+		if (alleSpielerIds[i] != player) {
+			players.push(alleSpielerIds[i]);
+		}
+	}
+	return players;
+}
+function getPossibleButtons() {
+	var buttons = [];
+	if (anzeigeAnsagen) {
+		buttons.push('re', 'kontra');
+	}
+	buttons.push('keine90', 'keine60', 'keine30', 'schwarz');
+	if (anzeigeSonderpunkt) {
+		buttons.push('sonderpunkt');
+	}
+	if (anzeigeStilleHochzeit) {
+		buttons.push('stillehochzeit');
+	}
+	buttons.push('abbrechen');
+	return buttons;
+}
+function allButtons() {
+	return {
+		kreuzsolo : {
+			farbe : 'btn-primary',
+			text : 'Farbsolo (Kreuz)'
+		},
+		piksolo : {
+			farbe : 'btn-primary',
+			text : 'Farbsolo (Pik)'
+		},
+		herzsolo : {
+			farbe : 'btn-primary',
+			text : 'Farbsolo (Herz)'
+		},
+		karosolo : {
+			farbe : 'btn-primary',
+			text : 'Farbsolo (Karo)'
+		},
+		damensolo : {
+			farbe : 'btn-primary',
+			text : 'Damensolo'
+		},
+		bubensolo : {
+			farbe : 'btn-primary',
+			text : 'Bubuensolo'
+		},
+		fleischlos : {
+			farbe : 'btn-primary',
+			text : 'Fleischloser'
+		},
 		re : {
 			farbe : 'btn-primary',
 			text : 'Ansage: Re'
@@ -44,20 +232,37 @@ function createButton(button) {
 			farbe : 'btn-warning',
 			text : 'Schwarz'
 		},
+		abrechnung : {
+			farbe : 'btn-success',
+			text : 'Spielabrechnung'
+		},
+		beenden : {
+			farbe : 'btn-danger',
+			text : 'Runde beenden'
+		},
 		abbrechen : {
 			farbe : 'btn-danger',
 			text : 'Abbrechen'
+		},
+		doppelkopf : {
+			farbe : 'btn-primary',
+			text : 'Doppelkopf'
+		},
+		fuchsgefangen : {
+			farbe : 'btn-primary',
+			text : 'Fuchs gefangen'
+		},
+		karlchen : {
+			farbe : 'btn-primary',
+			text : 'Karlchen'
+		},
+		karlchengefangen : {
+			farbe : 'btn-primary',
+			text : 'Karlchen gefangen'
 		}
 	};
-	return '<button type="button" class="btn ' + buttons[button]['farbe']
-			+ ' btn-block" id="' + button + '">' + buttons[button]['text']
-			+ '</button>';
 }
-function createMenu(menuId, buttons) {
-	var return_value = '<span id="' + menuId + '">';
-	for (i = 0; i < buttons.length; i++) {
-		return_value += createButton(buttons[i]);
-	}
-	return_value += '</span>';
-	return return_value;
-}
+/*
+ * $('.parent-element').on('click', '.mylink', function(){ alert ("new link
+ * clicked!"); })
+ */
--- templates/runde_spiele.tpl
+++ templates/runde_spiele.tpl
@@ -1,29 +1,16 @@
-<script src="./javascript/runde_javascript.js"></script>
-
 <script>
-spielTyp = 1;
-var menu = 'ms1';
 var alleSpieler = {ldelim}{foreach $alleSpieler as $spielerId => $spieler}{if !$spieler@first}, {/if}{$spielerId}: '{$spieler.vorname}'{/foreach}{rdelim};
+var alleSpielerIds = [{foreach $alleSpieler as $spielerId => $spieler}{if !$spieler@first}, {/if}{$spielerId}{/foreach}];
+var alleSpielerNamen = [{foreach $alleSpieler as $spielerId => $spieler}{if !$spieler@first}, {/if}'{$spieler.vorname}'{/foreach}];
+
 
 </script>
 
-<h3>Spiel {$aktuellesSpiel} - Geber: {$geber}</h3>
+<h3>Spiel {$aktuellesSpiel}</h3>
+<p class="lead">Geber:<span class="float-right">{$geber}</span></p>
 <div class="row">
-	<div class="col-sm-12 col-md-3 mt-3" id="menu">
-		<span id="ms1"> {foreach $alleSpieler as $spielerId => $spieler}
-			<button type="button" class="btn btn-primary btn-block" id="{$spielerId}">{$spieler.vorname}</button> {/foreach}
-			<button type="button" class="btn btn-success btn-block">Spielabrechnung</button>
-			<button type="button" class="btn btn-danger btn-block">Runde beenden</button>
-		</span>
+	<div class="col-sm-12 col-md-3" id="menu">
 	</div>
 </div>
-<script>
-$("#ms1 > button.btn").on("click", function(event){
-	var spielerId = this.id;
-	if(spielTyp) {
-		$('#menu').html(createMenu('ms3',['re','kontra','keine90','keine60','keine30','schwarz','sonderpunkt','stillehochzeit','abbrechen']));
-	} else {
-		$('#menu').html(createMenu('ms2',['re','kontra','sonderpunkt','hochzeit','armut','solo','abbrechen']));
-	}
-});
-</script>
+
+<script src="./javascript/runde_javascript.js"></script>
